b7d7cf59ab5bd56bb7d46d4c222d919bc221b812
MAZOUZ ABDERRAHMANE <mazathomazigh@gmail.com>
2026-04-11 17:43:45 +0100

    i wake the uploaded image display in web site

---
+++ script.js
@@ -0,0 +1,25 @@
+let imageInput = null;
+
+// On crée la machine (fonction) pour transformer l'image en texte (Base64)
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
+document.getElementById("icon").src="upload-icon.svg";
+
+// On ajoute 'async' ici car la transformation prend un petit peu de temps
+document.getElementById("image").addEventListener("input", async function (event) {
+    imageInput = event.target.files[0];
+    const url = URL.createObjectURL(imageInput);
+    document.getElementById("icon").src = url;
+    document.getElementById("icon").style.cssText = 'width: 95%; height: 95%;';
+
+    // On utilise 'await' pour attendre que la machine finisse son travail
+    const base64 = await toBase64(imageInput);
+    console.log(base64);
+});
+
+
