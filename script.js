82d9981645c8610750341ace67860f4abcfd9424
MAZOUZ ABDERRAHMANE <mazathomazigh@gmail.com>
2026-04-15 12:36:31 +0100

    the IA model works

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let imageInput = null;
+import { GoogleGenAI } from "https://esm.run/@google/genai";
 
+let imageInput = null;
 // On crée la machine (fonction) pour transformer l'image en texte (Base64)
 const toBase64 = file => new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -12,14 +13,34 @@ document.getElementById("icon").src="upload-icon.svg";
 
 // On ajoute 'async' ici car la transformation prend un petit peu de temps
 document.getElementById("image").addEventListener("input", async function (event) {
+    const base64 = await toBase64(imageInput);
+    
     imageInput = event.target.files[0];
     const url = URL.createObjectURL(imageInput);
     document.getElementById("icon").src = url;
     document.getElementById("icon").style.cssText = 'width: 95%; height: 95%; border-radius: 10px;';
 
     // On utilise 'await' pour attendre que la machine finisse son travail
-    const base64 = await toBase64(imageInput);
-    console.log(base64);
+    const ai = new GoogleGenAI({ apiKey: "" });
+    
+    const contents = [
+  {
+    inlineData: {
+      mimeType: imageInput.type,
+      data: base64.split(",")[1],
+    },
+  },
+  "Caption this image.",
+];
+
+const response = await ai.models.generateContent({
+  model: "gemini-2.5-flash",
+  contents: contents,
+});
+console.log(response.text);
+
+
+
 });
 
 
